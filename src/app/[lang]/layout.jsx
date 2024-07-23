f1f759534b2fa954e8239b3f342a93b34a6cff96
hroomdeveloper <hroomdeveloper@gmail.com>
2024-07-23 12:37:46 +0300

    title
    changed

--- src/app/[lang]/layout.jsx
+++ src/app/[lang]/layout.jsx
@@ -19,16 +19,14 @@ import '@/app/globals.css'
 import '@assets/iconify-icons/generated-icons.css'
 
 export const metadata = {
-  title: 'Materio - Material Design Next.js Admin Template',
-  description:
-    'Materio - Material Design Next.js Admin Dashboard Template - is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.'
+  title: 'HROOM',
+  description: 'HROOM - HROOM Intelligent Design Service based on MUI v5.'
 }
 
 const RootLayout = ({ children, params }) => {
   // Vars
   const headersList = headers()
   const direction = i18n.langDirection[params.lang]
-
 
   console.log('root layout params.lang' + params.lang)
 
